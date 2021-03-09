9e98a6eb11ad419290b2aa8b5138cb8c8bfd8e61
kellydinneen <kgdinneen@gmail.com>
2021-03-09 11:48:25 -0700

    Complete stubbed post test

--- cypress/integration/form-spec.js
+++ cypress/integration/form-spec.js
@@ -1,5 +1,26 @@
-describe('Submission', () => {
+describe('Form', () => {
   beforeEach(() => {
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3001/api/v1/urls'
+    },
+    {
+      statusCode: 201,
+      body: {urls: [
+        {
+          id: 1,
+          long_url: "https://www.google.com/",
+          short_url: "http://localhost:3001/useshorturl/1",
+          title: 'testOne'
+        },
+        {
+          id: 2,
+          long_url: "https://www.google.com/",
+          short_url: "http://localhost:3001/useshorturl/2",
+          title: 'testTwo'
+        }
+      ]}
+    })
     cy.visit('http://localhost:3000');
   })
 
@@ -17,23 +38,56 @@ describe('Submission', () => {
       .should('have.value', 'https://www.google.com/')
   })
 
+});
+
+describe('Form Submission', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000');
+  })
+
   it('Should be able to see new url rendered on submit', () => {
     cy.intercept('POST', 'http://localhost:3001/api/v1/urls', {
       statusCode: 200,
       body: {
-        id: 1,
+        id: 3,
         long_url: "https://www.google.com/",
-        short_url: "http://localhost:3001/useshorturl/1",
-        title: 'test'
+        short_url: "http://localhost:3001/useshorturl/3",
+        title: 'testPOST'
       }
+    })
+    cy.intercept({
+      method: 'GET',
+      url: 'http://localhost:3001/api/v1/urls'
+    },
+    {
+      statusCode: 201,
+      body: {urls: [
+        {
+          id: 1,
+          long_url: "https://www.google.com/",
+          short_url: "http://localhost:3001/useshorturl/1",
+          title: 'testOne'
+        },
+        {
+          id: 2,
+          long_url: "https://www.google.com/",
+          short_url: "http://localhost:3001/useshorturl/2",
+          title: 'testTwo'
+        },
+        {
+          id: 3,
+          long_url: "https://www.google.com/",
+          short_url: "http://localhost:3001/useshorturl/3",
+          title: 'testPOST'
+        }
+      ]}
     })
     cy.get('form').find('.title-input').type('test')
     cy.get('form').find('.url-input').type('https://www.google.com/')
-    cy.get('form').find('.submit-button').click().wait('2000')
-    cy.get('url-container').children().last().should('have.class', 'test')
-    cy.get('url-container')
+    cy.get('form').find('.submit-button').click().wait(2000)
+    cy.get('.url-container')
       .children().last()
         .children().first()
-        .should('have.text', 'test')
+        .should('have.text', 'testPOST')
   })
 });
